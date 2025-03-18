95539d0d14fafe210d6c8813b5b8f9b4de6a4fc3
Ali Shan <alishanamin125@gmail.com>
2025-03-18 17:13:22 +0500

    add my code chsnges

---
+++ CLASS4/classone.js
@@ -0,0 +1,63 @@
+console.log("Hello World!")
+console.log(12+12)
+
+//VARIABLES
+
+var a = 12;
+var b = 15;
+console.log(a + b)
+a = b + 50;
+console.log(a);
+
+//DATA TYPES
+// string       "A", "123", "hello world"
+// number
+// boolean
+// undefined
+// null
+// symbol
+// bigint
+
+var firstName;         //Declare variable
+firstName = "Ali"      //Assign value to variable
+console.log(firstName) //Printing the value of variable
+firstName = "Alex"        //string
+console.log(typeof firstName)
+
+var myAge;
+myAge = 19;
+console.log(myAge)        //number
+
+var isMale = true;        //boolean
+
+var address = undefined;  //undefined
+console.log(address)
+
+var hello = null;
+console.log(typeof hello) // null
+
+var id = Symbol("A")
+var id2 = Symbol("B")
+console.log(typeof id)    // symbol
+
+console.log(Number.MAX_SAFE_INTEGER) // bigint
+var bg = 99988232132660046246n
+var bg1 = BigInt(99988232132660046246)
+console.log(typeof bg)
+console.log(bg1)
+
+
+
+
+var a = 10
+var b = a
+console.log(a, b)
+a=100
+
+
+
+
+
+b=1000000
+
+
