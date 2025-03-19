12fa6a8dcf846cd3a9455ce78c0fb54385b34187
Ali Shan <alishanamin125@gmail.com>
2025-03-19 16:05:40 +0500

    classFive

---
+++ CLASS5/classFive.js
@@ -0,0 +1,16 @@
+// Non Primitive Datatype
+// Big Data
+
+// Arrays
+
+var fruits = ["Apple", "Mango", "Banana"]
+console.log(fruits)
+
+var user = {
+    name: "John",
+    age: 21,
+    email: "abcd@gmail.com",
+    city: "Islamabad",
+    country: "Pakistan"
+}
+console.log(user)
