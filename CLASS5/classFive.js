835ee72b3d6c6c7374c10451fbd5797520c012d6
Ali Shan <alishanamin125@gmail.com>
2025-03-30 11:56:30 +0500

    classFive

--- CLASS5/classFive.js
+++ CLASS5/classFive.js
@@ -1,135 +1,135 @@
-// // NOn primitive dt /  reference dt
-// //  big data
+// Non Primitive Datatype /  Reference Datatype
+//  Big Data
+// Arrays
 
-// // Arrays
-// // _______________________
+// _______________________
 
-// var fruits = ["Apple" , "banana" , "grapes", 21 , true , 12n , undefined , null , [1,2,3]]
+var fruits = ["Apple" , "banana" , "grapes", 21 , true , 12n , undefined , null , [1,2,3]]
 
 // objects
 
-// var user = {
-//     name:"John",
-//     age:21,
-//     email:"abc@example.com",
-//     city:"Gilgit",
-//     country:"Pakistan"
-// }
+var user = {
+    name:"John",
+    age:21,
+    email:"abc@example.com",
+    city:"Gilgit",
+    country:"Pakistan"
+}
 
-// var users = [
-//   {
-//     name: "John",
-//     age: 21,
-//     email: "abc@example.com",
-//     city: "Gilgit",
-//     country: "Pakistan",
-//   },
-//   {
-//     name: "alex",
-//     age: 23,
-//     email: "adsdf@example.com",
-//     city: "Gilgit",
-//     country: "Pakistan",
-//   },
-// ];
+var users = [
+  {
+    name: "John",
+    age: 21,
+    email: "abc@example.com",
+    city: "Gilgit",
+    country: "Pakistan",
+  },
+  {
+    name: "alex",
+    age: 23,
+    email: "adsdf@example.com",
+    city: "Gilgit",
+    country: "Pakistan",
+  },
+];
 
-// console.log(users);
-// var users = [
-//       {
-//         name: "John",
-//         age: 21,
-//         email: "abc@example.com",
-//         city: "Gilgit",
-//         country: "Pakistan",
-//       },
-//       {
-//         name: "alex",
-//         age: 23,
-//         email: "adsdf@example.com",
-//         city: "Gilgit",
-//         country: "Pakistan",
-//       },
-//     ];
-
-
-
-//     var nums = [1,2,3,4];
-//     console.log(nums , nums.length)
-
-// var gn = nums[3] +6766
-// console.log(gn)
-// console.log(nums)
-
-// var key1 = 'id'
-// var product = {
-//     [key1]:3234554666, 
-//     pname:"Shoes",
-//     price:1799,
-//     discount:10,
-//     rating :4.5
-// }
-// var productName= product.pname
-// bracket 
-// var productName = product[pname]
-// console.log(product)
-// console.log(product[key1])
-
-
-// const arr= ["a" ,"b" , "c"];
-// console.log(arr)
-// // get
-// console.log(arr[0])
-// // update
-// arr[0] = "updated"
-// console.log(arr[0])
-// console.log(arr)
-// //add
-// arr[3] ="adding"
-// console.log(arr)
+console.log(users);
+var users = [
+      {
+        name: "John",
+        age: 21,
+        email: "abc@example.com",
+        city: "Gilgit",
+        country: "Pakistan",
+      },
+      {
+        name: "alex",
+        age: 23,
+        email: "adsdf@example.com",
+        city: "Gilgit",
+        country: "Pakistan",
+      },
+    ];
 
 
 
+    var nums = [1,2,3,4];
+    console.log(nums , nums.length)
 
-// var product = {
-//     id:3234554666, 
-//     pname:"Shoes",
-//     price:1799,
-//     discount:10,
-//     rating :4.5
-// }
+var gn = nums[3] +6766
+console.log(gn)
+console.log(nums)
 
-// // get 1> bracket 
-// console.log(product.id)
-
-// // get 2> bracket
-// console.log(product["id"])
-
-// // values update
-// product.id =233244
-// console.log(product)
-
-
-// add
-// product.stock = 123
-// console.log(product)
+var key1 = 'id'
+var product = {
+    [key1]:3234554666, 
+    pname:"Shoes",
+    price:1799,
+    discount:10,
+    rating :4.5
+}
+var productName= product.pname
+bracket 
+var productName = product[pname]
+console.log(product)
+console.log(product[key1])
 
 
-// // computed properties 
+const arr= ["a" ,"b" , "c"];
+console.log(arr)
+// get
+console.log(arr[0])
+// update
+arr[0] = "updated"
+console.log(arr[0])
+console.log(arr)
+//add
+arr[3] ="adding"
+console.log(arr)
 
 
-// var users = [
-//       {
-//         name: "John",
-//         age: 21,
-//         email: "abc@example.com",
-//         city: "Gilgit",
-//         country: "Pakistan",
-//         address:{
-//             street:"123 Main St",
-//             city:"Gilgit",
-//             country:"Pakistan"
-//         }
-//       }
-//     ];
 
-//     console.log(users.length)
+
+var product = {
+    id:3234554666, 
+    pname:"Shoes",
+    price:1799,
+    discount:10,
+    rating :4.5
+}
+
+// get 1> bracket 
+console.log(product.id)
+
+// get 2> bracket
+console.log(product["id"])
+
+// values update
+product.id =233244
+console.log(product)
+
+
+add
+product.stock = 123
+console.log(product)
+
+
+// computed properties 
+
+
+var users = [
+      {
+        name: "John",
+        age: 21,
+        email: "abc@example.com",
+        city: "Gilgit",
+        country: "Pakistan",
+        address:{
+            street:"123 Main St",
+            city:"Gilgit",
+            country:"Pakistan"
+        }
+      }
+    ];
+
+    console.log(users.length)
