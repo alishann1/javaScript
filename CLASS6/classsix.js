d7f1488f8896868bef0209e311d3e8f31cac7410
Ali Shan <alishanamin125@gmail.com>
2025-04-01 20:32:57 +0500

    classSix

--- CLASS6/classsix.js
+++ CLASS6/classsix.js
@@ -1,115 +1,114 @@
-// // Array Destructuring
+// Array Destructuring
 
-// var myArray = ["apple", "mango", "grape", "banana"]
+var myArray = ["apple", "mango", "grape", "banana"]
 
-// console.log(myArray)
-// myArray[0] = "zero"
-// console.log(myArray)
+console.log(myArray)
+myArray[0] = "zero"
+console.log(myArray)
 
-// // var item1 = myArray[0];
-// // var item2 = myArray[1];
-// // var item3 = myArray[2];
-// // var item4 = myArray[3];
+// var item1 = myArray[0];
+// var item2 = myArray[1];
+// var item3 = myArray[2];
+// var item4 = myArray[3];
 
-// // The above whole process has been done in a single line below using destructuring
+// The above whole process has been done in a single line below using destructuring
 
-// var [item1, item2, item3, item4] = myArray
-// console.log(item1, item2, item3, item4)
-// console.log(item4)
-
-
-// // Object Destructuring
-
-// var user = {
-//     name: "Ali",
-//     id: 5698,
-//     age: 21,
-// }
-
-// // We can change the key variable name to whatever we want by the below method
-
-// var { name: userName, id: userId, age: userAge } = user
-
-// console.log(userAge, userId)
+var [item1, item2, item3, item4] = myArray
+console.log(item1, item2, item3, item4)
+console.log(item4)
 
 
+// Object Destructuring
 
-// // Nested Destructuring 
+var user = {
+    name: "Ali",
+    id: 5698,
+    age: 21,
+}
 
+// We can change the key variable name to whatever we want by the below method
 
-// var nested =
-//     [
-//         {
-//             userName: "John",
-//             id: 1234
-//         }
-//         ,
-
-//         {
-//             stdName: "Ali",
-//             stdId: 5698
-//         }
-//     ]
-
-// var myVar = nested[1].stdName
-// console.log(myVar)
+var { name: userName, id: userId, age: userAge } = user
+console.log(userAge, userId)
 
 
-// var users = [
-//     {
-//         id: 2344,
-//         fullname: "John",
-//         age: 30,
-//         address: {
-//             city: "London",
-//             country: "UK"
-//         }
-//     }
 
-// ]
+// Nested Destructuring 
 
-// var [{ address: { country: userCountry } }] = users
-// console.log(userCountry)
 
-// // ------------------------------------------------
+var nested =
+    [
+        {
+            userName: "John",
+            id: 1234
+        }
+        ,
 
-// // Function Declaration
+        {
+            stdName: "Ali",
+            stdId: 5698
+        }
+    ]
 
-// function sum() {
-//     console.log(15 + 5)
-//     return [1, 2, 3]
-// }
-// var result = sum()
-// console.log(result)
+var myVar = nested[1].stdName
+console.log(myVar)
 
-// // Function Expression
 
-// var divide = function (a, b) {
-//     if (b == 0) {
-//         console.log("Cannot divide by zero")
-//     }
-//     else {
-//         return a / b;
-//     }
+var users = [
+    {
+        id: 2344,
+        fullname: "John",
+        age: 30,
+        address: {
+            city: "London",
+            country: "UK"
+        }
+    }
 
-// }
-// console.log(divide(10, 2))
+]
 
-// // Arrow Function
+var [{ address: { country: userCountry } }] = users
+console.log(userCountry)
 
-// var greet = (name) => {
-//     console.log("Hello", name)
-// }
-// greet("John!")
+// ------------------------------------------------
 
-// var add = (a, b) => {
-//     return a + b;
-// }
-// var res1 = add(50, 20)
-// var res2 = add(60, 30)
-// var res3 = add(70, 55)
+// Function Declaration
 
-// console.log(res1, res2)
+function sum() {
+    console.log(15 + 5)
+    return [1, 2, 3]
+}
+var result = sum()
+console.log(result)
+
+// Function Expression
+
+var divide = function (a, b) {
+    if (b == 0) {
+        console.log("Cannot divide by zero")
+    }
+    else {
+        return a / b;
+    }
+
+}
+console.log(divide(10, 2))
+
+// Arrow Function
+
+var greet = (name) => {
+    console.log("Hello", name)
+}
+greet("John!")
+
+var add = (a, b) => {
+    return a + b;
+}
+var res1 = add(50, 20)
+var res2 = add(60, 30)
+var res3 = add(70, 55)
+
+console.log(res1, res2)
 
 
 const books = [
