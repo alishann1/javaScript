ab7b1ed2c6870fa86132c596983c1df1329deeac
Ali Shan <alishanamin125@gmail.com>
2025-04-05 19:26:43 +0500

    classFourteen

--- CLASS14/classfourteen.js
+++ CLASS14/classfourteen.js
@@ -1,144 +1,144 @@
-// const methods = {
-//     getName: function () {
-//         return this.fname
-//     }
-// }
+const methods = {
+    getName: function () {
+        return this.fname
+    }
+}
 
-// function createUser(fname, email, age) {
-//     // 1
-//     let user = Object.create(methods)  //{} , chain --> methods --> proto
-//     console.log("step one", user)
-//     // 2
-//     user.fname = fname
-//     user.email = email
-//     user.age = age
+function createUser(fname, email, age) {
+    // 1
+    let user = Object.create(methods)  //{} , chain --> methods --> proto
+    console.log("step one", user)
+    // 2
+    user.fname = fname
+    user.email = email
+    user.age = age
 
-//     console.log("step two", user)
-//     //3
-//     console.log("step three", user)
-//     return user
-// }
+    console.log("step two", user)
+    //3
+    console.log("step three", user)
+    return user
+}
 
-// const user1 = createUser("alex", "alex@gmail.com", 23);
-// console.log(user1)
+const user2 = createUser("alex", "alex@gmail.com", 23);
+console.log(user2)
 
-// // ______________________________________________________
+// ______________________________________________________
 
-// function hello() {
-//     console.log("hello")
+function hello() {
+    console.log("hello")
 
-// }
-
-
-// console.log(hello.name)
-// hello.call()
+}
 
 
-// hello.fname = "alex";
-
-// console.log(hello.prototype)
-
+console.log(hello.name)
+hello.call()
 
 
+hello.fname = "alex";
 
-// function createUser(fname, email, age) {
-//     //   1
-//     const user = Object.create(createUser.prototype);  // {} + {}
-
-//     // 2  
-//     user.fname = fname
-//     user.email = email
-//     user.age = age
-//     //  {fname:"" , email:"" , age:"" } + {}
-
-
-//     // 3
-//     return user    // {fname:"" , email:"" , age:"" } + {}
+console.log(hello.prototype)
 
 
 
-// }
+
+function createUser(fname, email, age) {
+    //   1
+    const user = Object.create(createUser.prototype);  // {} + {}
+
+    // 2  
+    user.fname = fname
+    user.email = email
+    user.age = age
+    //  {fname:"" , email:"" , age:"" } + {}
 
 
-// //4
-// createUser.prototype.getName = function () {
-//     console.log(this.fname);
-// }
-
-
-// createUser.prototype.setName = function (updatedName) {
-//     this.fname = updatedName;
-// }
-
-
-
-// const u1 = createUser("alex", "a@example.com", 24)
-
-// console.log(u1)
-
-// u1.setName("john")
-// console.log(u1)
+    // 3
+    return user    // {fname:"" , email:"" , age:"" } + {}
 
 
 
-// function CreateUser(fname, age) {
-//     console.log(this)
-//     this.fname = fname;
-//     this.age = age;
-// }
-
-// CreateUser.prototype.sing = function () {
-//     console.log("lalala la a")
-// }
+}
 
 
-// const u1 = new CreateUser("alex", 23)
-// console.log(u1)
-// u1.sing()
+//4
+createUser.prototype.getName = function () {
+    console.log(this.fname);
+}
 
-// // new String
+
+createUser.prototype.setName = function (updatedName) {
+    this.fname = updatedName;
+}
 
 
 
-// // OOPS
-// // CLASS
-// // ___________________________________________
+const u1 = createUser("alex", "a@example.com", 24)
 
-// class User {
-//     constructor(fname, age, email) {
-//         this.fname = fname;
-//         this.age = age;
-//         this.email = email;
-//     }
+console.log(u1)
 
-
-//     getName() {
-//         console.log(this.fname);
-//     }
+u1.setName("john")
+console.log(u1)
 
 
 
-// }
+function CreateUser(fname, age) {
+    console.log(this)
+    this.fname = fname;
+    this.age = age;
+}
 
-// const user1 = new User("ALex", 21, "a@gmail.com")
-// console.log(user1)
-
-
-// // ____________________________________________
-
-// // pop , push , shift , unshift
-// const arr = [1, 2, 3, 4, 5]
-// arr.pop()
-// arr.shift()
+CreateUser.prototype.sing = function () {
+    console.log("lalala la a")
+}
 
 
-// arr.push(6)
-// arr.unshift(6)
-// console.log(arr)
+const u2 = new CreateUser("alex", 23)
+console.log(u2)
+u1.sing()
+
+// new String
 
 
 
-// slice
+// OOPS
+// CLASS
+// ___________________________________________
 
-// const a2 = arr.slice(0, 3)
-// console.log(a2)
+class User {
+    constructor(fname, age, email) {
+        this.fname = fname;
+        this.age = age;
+        this.email = email;
+    }
+
+
+    getName() {
+        console.log(this.fname);
+    }
+
+
+
+}
+
+const user1 = new User("ALex", 21, "a@gmail.com")
+console.log(user1)
+
+
+// ____________________________________________
+
+// pop , push , shift , unshift
+const arr = [1, 2, 3, 4, 5]
+arr.pop()
+arr.shift()
+
+
+arr.push(6)
+arr.unshift(6)
+console.log(arr)
+
+
+
+slice
+
+const a2 = arr.slice(0, 3)
+console.log(a2)
