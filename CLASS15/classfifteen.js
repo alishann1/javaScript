014f09a4af75d26a8c00fbc5f83300755e5c646d
Ali Shan <alishanamin125@gmail.com>
2025-04-08 15:05:32 +0500

    classFifteen

--- CLASS15/classfifteen.js
+++ CLASS15/classfifteen.js
@@ -1,14 +1,14 @@
 // Classes
 
 class Animal {
-      
-    constructor(name  , age  , color){
+
+    constructor(name, age, color) {
         this.name = name;
         this.age = age;
         this.color = color;
     }
 
-    eat(){
+    eat() {
         console.log("Eating...")
     }
 
@@ -17,24 +17,24 @@ class Animal {
 // const a1= new Animal("dog" , 2 ,  "white")
 // const a2= new Animal("car" , 1 ,  "black")
 
-class Dog  extends Animal {
-     constructor(price, name , age , color){
-         super(name, age, color)
-         this.price = price
-     }
+class Dog extends Animal {
+    constructor(price, name, age, color) {
+        super(name, age, color)
+        this.price = price
+    }
 }
 
 
 class Cat extends Animal {
-   constructor(name, age, color, sound){
-         super(name, age, color)
-         this.sound = sound
-   }
+    constructor(name, age, color, sound) {
+        super(name, age, color)
+        this.sound = sound
+    }
 }
 
-const dog1 = new Dog(12000, "dog1" , 1 , "gray" )
+const dog1 = new Dog(12000, "dog1", 1, "gray")
 dog1.eat()
-const cat1 = new Cat("cat1" , 1, "brown" , "meowww")
+const cat1 = new Cat("cat1", 1, "brown", "meowww")
 console.log(cat1)
 
 
@@ -48,33 +48,33 @@ console.log(cat1)
 
 
 
-// encapsulation
+// Encapsulation
 
 
-class Bank{
+class Bank {
 
-    // private properties
-    #balance  = 0
-  constructor(accountName , accountid , accountType){
-    this.accountName = accountName;
-    this.accountid = accountid;
-    this.accountType = accountType;
-  }
+    // Private properties
+    #balance = 0
+    constructor(accountName, accountid, accountType) {
+        this.accountName = accountName;
+        this.accountid = accountid;
+        this.accountType = accountType;
+    }
 
-    deposit(amount, role){
+    deposit(amount, role) {
 
-        if(role === "admin"){
+        if (role === "admin") {
 
             this.#balance += amount
             console.log(`Deposited ${amount} successfully`)
-        }else{
+        } else {
             console.log("Only admins can deposit")
         }
 
     }
 
 
-    balanceInfo(){
+    balanceInfo() {
         console.log(`The balance is ${this.#balance}`)
     }
 
@@ -84,7 +84,7 @@ class Bank{
 }
 
 
-const acc1 = new Bank("John" , 1234 , "saving")
+const acc1 = new Bank("John", 1234, "saving")
 acc1.deposit(1000, "admin")
 acc1.balanceInfo()
 console.log(acc1.balanceInfo())
@@ -108,7 +108,7 @@ console.log(acc1.balanceInfo())
 console.log(Math)
 // abs
 
-var a= 2
+var a = 2
 
 // var b  = Math.abs(a)
 // console.log(b)
@@ -178,14 +178,14 @@ var a= 2
 // clousers
 
 
-function outerFUnc(){
-    counter =0
-    return function innerfunc(){
+function outerFUnc() {
+    counter = 0
+    return function innerfunc() {
         console.log(counter++)
     }
 }
 
-const f= outerFUnc()
+const f = outerFUnc()
 
 f()
 f()
