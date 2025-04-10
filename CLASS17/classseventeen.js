63638c89724f894f752aa7514818ba1b9bc3ed70
Ali Shan <alishanamin125@gmail.com>
2025-04-10 19:16:05 +0500

    classSeventeen

--- CLASS17/classseventeen.js
+++ CLASS17/classseventeen.js
@@ -1,10 +1,10 @@
 // Attributes 
-const box  = document.querySelector(".box")
+const box = document.querySelector(".box")
 
 box.setAttribute("id", "box1")
-box.setAttribute("title" , "this is main box")
+box.setAttribute("title", "this is main box")
 const res = box.getAttribute("id")
-console.log(box , res)
+console.log(box, res)
 box.removeAttribute("id")
 
 
@@ -12,20 +12,20 @@ box.removeAttribute("id")
 // Class
 
 console.log(box.classList)
-add
+// Add
 box.classList.add("mainboxxx")
 
-contains 
+// Contains
 const r = box.classList.contains("box")
 console.log(r)
 
-replace
-box.classList.replace("box1" , "box01")
+// Replace
+box.classList.replace("box1", "box01")
 
 
 
 // Remove
-
+// h4.remove()
 
 // Toogle
 box.classList.toggle("box0001")
@@ -35,43 +35,42 @@ console.log(box.classList)
 
 const body = document.querySelector("body")
 
-// create elements using js 
-document
-const h4= document.createElement("h4") 
-{/* <h4></h4> */}
+// create elements using js document
+
+const h4 = document.createElement("h4")
+{/* <h4></h4> */ }
 h4.textContent = "using js to create h4"
-{/* <h4>using js to create h4</h4> */}
+{/* <h4>using js to create h4</h4> */ }
 
 body.prepend(h4)
-body.append(h4)
-box.before(h4)
-box.after(h4)
-console.log(h4)
-h4.remove()
+// body.append(h4)
+// box.before(h4)
+// box.after(h4)
+// console.log(h4)
+
 
 
 
 // Adjacent 
 // beforebegin  afterbegin , beforeend , afterend
-box.insertAdjacentElement("beforebegin" , h4)
-
-box.insertAdjacentElement("afterbegin" , h4)
-box.insertAdjacentElement("beforeend" , h4)
-box.insertAdjacentElement("afterend" , h4)
+box.insertAdjacentElement("beforebegin", h4)
+box.insertAdjacentElement("afterbegin", h4)
+box.insertAdjacentElement("beforeend", h4)
+box.insertAdjacentElement("afterend", h4)
 
 //  
 
 // console.log("start")
 
-for (let i = 0; i < 10000; i++) {
-    console.log("1")
-}
+// for (let i = 0; i < 10000; i++) {
+//     console.log("1")
+// }
 const btn = document.getElementById("myButton")
 console.log(btn)
-btn.addEventListener("mouseleave" , ()=>{
-    if(box.style.backgroundColor === "green"){
+btn.addEventListener("mouseup", () => {
+    if (box.style.backgroundColor === "green") {
         box.style.backgroundColor = "white"
-    }else{
+    } else {
         box.style.backgroundColor = "green"
     }
 
@@ -81,10 +80,8 @@ btn.addEventListener("mouseleave" , ()=>{
 console.log("end")
 
 // keyup , keydown
-window.addEventListener("keyup" ,  (e)=>{
-   box.textContent =   `you pressed key  ${e.key}`
-
-
+window.addEventListener("keyup", (e) => {
+    box.textContent = `you pressed key  ${e.key}`
 })
 
 
